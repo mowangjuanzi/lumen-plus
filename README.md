8e965aac92c22ca5d2c50eafff2f201226dd5e2d
mowangjuanzi <baoguoxiao0538@hotmail.com>
2020-02-25 08:38:58 +0800

    新增一些make命令

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # lumen-plus
 lumen feature plus
+
+## 简介
+
+本组件是作为 Lumen 框架服务的增强
+
+## 功能
+
+1. 新增以下 `make` 命令
+
+- `make:controller`
+- `make:model`
+- `make:factory`
+
+## 安装
+
+执行以下命令
+
+```bash
+composer require mowangjuanzi/lumen-plus
+```
+
+注册服务提供者
+
+```php
+$app->register(Mowangjuanzi\Plus\LumenServiceProvider::class);
+```
---
+++ src/LumenServiceProvider.php
@@ -0,0 +1,82 @@
+<?php
+namespace Mowangjuanzi\Plus;
+
+use Illuminate\Database\Console\Factories\FactoryMakeCommand;
+use Illuminate\Support\ServiceProvider;
+use Mowangjuanzi\Plus\Console\ControllerMakeCommand;
+use Mowangjuanzi\Plus\Console\ModelMakeCommand;
+
+class LumenServiceProvider extends ServiceProvider
+{
+    /**
+     * The commands to be registered.
+     *
+     * @var array
+     */
+    protected $commands = [
+        'ControllerMake' => 'command.controller.make',
+        'FactoryMake' => 'command.factory.make',
+        'ModelMake' => 'command.model.make',
+    ];
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerCommands($this->commands);
+    }
+
+    /**
+     * Register the given commands.
+     *
+     * @param  array  $commands
+     * @return void
+     */
+    protected function registerCommands(array $commands)
+    {
+        foreach (array_keys($commands) as $command) {
+            call_user_func_array([$this, "register{$command}Command"], []);
+        }
+
+        $this->commands(array_values($commands));
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerControllerMakeCommand()
+    {
+        $this->app->singleton('command.controller.make', function ($app) {
+            return new ControllerMakeCommand($app['files']);
+        });
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerModelMakeCommand()
+    {
+        $this->app->singleton('command.model.make', function ($app) {
+            return new ModelMakeCommand($app['files']);
+        });
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerFactoryMakeCommand()
+    {
+        $this->app->singleton('command.factory.make', function ($app) {
+            return new FactoryMakeCommand($app['files']);
+        });
+    }
+}
